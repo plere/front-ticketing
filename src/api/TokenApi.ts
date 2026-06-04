b496bf5472f44f41a117c11ae51cc25f285aa059
ansy <plere0011@gmail.com>
2026-06-04 17:14:39 +0900

    WaitingApi -> TokenApi로 네이밍 변경 및 예매토큰 얻어오는 api 추가

--- src/api/TokenApi.ts
+++ src/api/TokenApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { setupInterceptors } from "./axiosInterceptor";
 import type { WaitingToken } from "./type/WaitingTokenResponse";
+import type { ReservationToken } from "../types/ReservationToken";
 
 const api = setupInterceptors(axios.create({
   baseURL: "http://localhost:8082", // Spring 서버
@@ -17,6 +18,12 @@ export const getPositionByWaitingTokenApi = async (waitingToken: WaitingToken): 
   const response = await api.get<{body: number}>(`/waiting-token/position/concerts`, {
     params: waitingToken
   });
+
+  return response.data.body;
+};
+
+export const getReservationTokenApi = async (waitingToken: WaitingToken): Promise<ReservationToken> => {
+  const response = await api.get<{body: ReservationToken}>(`/reservation-token/concerts/${waitingToken.id}/${waitingToken.token}`);
 
   return response.data.body;
 };
