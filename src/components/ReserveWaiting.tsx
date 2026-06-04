bbe93e21193ad6d56c2754281ea5c55e20ab656f
ansy <plere0011@gmail.com>
2026-06-04 17:16:50 +0900

    대기토큰 얻어온 후 예매토큰 얻어와 전역상태로 저장하는 로직 추가

--- src/components/ReserveWaiting.tsx
+++ src/components/ReserveWaiting.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import { getPositionByWaitingTokenApi, getWaitingTokenApi } from "../api/WaitingApi";
+import { getPositionByWaitingTokenApi, getReservationTokenApi, getWaitingTokenApi } from "../api/TokenApi";
 import { useNavigate, useParams } from "react-router-dom";
 import type { WaitingToken } from "../api/type/WaitingTokenResponse";
+import { reservationTokenStore } from "../stores/reservationTokenStore";
 
 export default function ReserveWaiting() {
   const navigate = useNavigate();
@@ -10,21 +11,14 @@ export default function ReserveWaiting() {
   const [position, setPosition] = useState(0);
   const [waitingToken, setWaitingToken] = useState<WaitingToken>();
   const intervalRef = useRef<number|null>(null);
+  const setReservationToken = reservationTokenStore((state) => state.setRservationToken)
 
   useEffect(() => {
-    const initApi = async () => {
-      const wt = await getWaitingTokenApi(Number(concertId), Number(roundId));
-      console.log("Wt",wt);
-      setWaitingToken(wt);
-      const p = await getPositionByWaitingTokenApi(wt);
-      setPosition(p);
-
-      if (p === 0) {          
-          stopPolling();
-        }
-    };
-
-    initApi();
+    getWaitingTokenApi(Number(concertId), Number(roundId))
+    .then(setWaitingToken)
+    .catch(err => {
+      console.log("getWaitingTokenApi error!!", err)
+    });
     }, []);
 
 
@@ -35,12 +29,18 @@ export default function ReserveWaiting() {
       intervalRef.current = null;
       console.log('폴링이 명시적으로 종료되었습니다.');
       if(position == 0) {
-        navigate(`/${roundId}/reserve`)
+        getReservationTokenApi(waitingToken!)
+        .then(res => {
+          setReservationToken(res);
+          navigate(`/${roundId}/reserve`);
+        })
       }
     }
   };
 
   useEffect(() => {
+    if(waitingToken == null) return;
+    
     const getPosition = async () => {
       try {
         const p = await getPositionByWaitingTokenApi(waitingToken!);
@@ -55,13 +55,14 @@ export default function ReserveWaiting() {
         stopPolling(); // 에러 발생 시에도 안전하게 종료
       }
     }
+    getPosition();
 
     // 4. 3초마다 반복 실행하도록 인터벌 등록 및 ref에 ID 저장
     intervalRef.current = setInterval(getPosition, 2000);
 
     // 5. 컴포넌트 언마운트 시 클린업
     return () => stopPolling();
-  }, []); // 빈 배열을 두어 최초 마운트 시에만 인터벌이 1번 등록되도록 함
+  }, [waitingToken]); // 빈 배열을 두어 최초 마운트 시에만 인터벌이 1번 등록되도록 함
 
   return (
     <>
